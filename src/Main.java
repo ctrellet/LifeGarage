15260e0ab30eaab004a5513714b11bcdfc74d3a4
ctrellet <christophe@trellet.ch>
2017-01-30 18:27:35 +0100

    Project initialisation

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by christophe on 30/01/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\n==========================================================");
+        System.out.println("       LifeGarage project ");
+        System.out.println("==========================================================");
+    }
+}

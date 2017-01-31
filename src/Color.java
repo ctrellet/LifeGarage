b23038e81c9599b0576b8717babde74e078a8ee8
ctrellet <christophe@trellet.ch>
2017-01-31 16:52:20 +0100

    Réenrichissement de l'enum "Color"

--- src/Color.java
+++ src/Color.java
@@ -2,18 +2,24 @@
  * Created by christophe on 30/01/17.
  */
 public enum Color {
-    RED("Red"),
-    GREEN("Green"),
-    BLUE("Blue");
+    RED(    "Red"   ,"FFFF00"),
+    GREEN(  "Green" ,"008000"),
+    BLUE(   "Blue"  ,"0000FF");
 
     private String name = "";
+    private String hexCode = "000000";
 
     //Constructeur
-    Color(String name){
+    Color(String name, String hexCode){
         this.name = name;
+        this.hexCode = hexCode;
     }
 
     public String toString(){
         return name;
+    }
+
+    public String getHexCode() {
+        return "#"+hexCode;
     }
 }
